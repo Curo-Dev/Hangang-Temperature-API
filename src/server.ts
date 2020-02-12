d6671663310033e7ff9a1adc709e44c0e4769a5c
cheongan <smartnation@naver.com>
2020-02-13 03:40:14 +0900

    feat: ⚡️ Add Result Value

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,15 @@ const app = express();
 
 app.get('/', function (req: express.Request, res: express.Response) {
   const tzO = new Date().getTimezoneOffset() * 60000; // Timezone 설정 (Seoul)
+  
   const date = new Date(Date.now() - tzO).toISOString().split("T")[0]; // 현재 시간 구하기
+  
 
   const options = { // request-promise 옵션 설정
     uri: `http://www.koreawqi.go.kr/wQDDRealTotalDataList_D.wq?item_id=M69&action_type=L&action_type_seq=1&auto_flag=&auto_site_id=S01007&search_data_type=1&search_flag2=1&river_id=R01&site_id=%27S01004%27&site_name=%B1%B8%B8%AE&search_interval=HOUR&search_date_from=${date}&search_date_to=${date}&order_type_1=MSR_DATE&order_type_2=ASC`,
     method: "GET",
     encoding: null, // null로 안 해주면 EUC-KR 오류 남.
+    timeout: 1000,
     headers: { // 헤더 부분 설정
       'User-Agent': 'HT-API/1.0',
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -25,20 +28,19 @@ app.get('/', function (req: express.Request, res: express.Response) {
     const $ = cheerio.load(body);
     const list = $("body > div > table > tbody > tr");         
     
+    if(list.length <= 3) res.json({ result: "NO_CONTENT" }); // 값 없음 {204}
+          
     const $$ = cheerio.load(list[list.length - 1]); // 제일 마지막 값 (최근 측정 온도)
     const date = $$("td:nth-child(1)").text().replace(/\s/g, "").substring(0, 10); // 날짜 부분
     const time = $$("td:nth-child(1)").text().replace(/\s/g, "").substring(10, 13); // 시간 부분
-    const tem = $$("td:nth-child(3)").text().trim(); // 온도 부분
+    const tem = $$("td:nth-child(3)").text().trim(); // 온도 부분    
 
-    const item = { // JSON 변환
-      date: date,
-      time: time,
-      tem: tem
-    }
-
-    res.json(item); // JSON 출력    
+    res.json( { result: "OK", date: date, time: time, tem: tem } ); // JSON 출력 {200}
   })    
-  .catch((err) => { console.log(err) });
+  .catch((err) => { 
+    console.log(err) // 오류 로그 출력
+    res.json({ result: "REQUEST_TIMEOUT" }); // 타임 아웃 {408}
+  });
 });
 
-app.listen(3000);
+app.listen(3000); // 서버 열기
